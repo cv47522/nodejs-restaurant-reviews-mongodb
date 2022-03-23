76bd5867cae30628927ce1ba0d4522ab20626606
Wan-Ting Hsieh <cv47522@gmail.com>
2022-03-23 19:29:18 +0200

    Add controllers

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -2,7 +2,7 @@ import RestaurantsDAO from "../dao/restaurantsDAO.js";
 
 export default class RestaurantsController {
     static async apiGetRestaurants(req,res, next) {
-        const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.);
+        const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20;
         const page = req.query.page ? parseInt(req.query.page, 10) : 0;
 
         let filters = {};
@@ -14,6 +14,34 @@ export default class RestaurantsController {
             filters.name = req.query.name;
         }
 
-        const { resaurantsList, totalRestaurants } = await RestaurantsDAO
+        const { restaurantsList, totalRestaurants } = await RestaurantsDAO.getRestaurants({
+            filters,
+            page,
+            restaurantsPerPage
+        });
+
+        let response = {
+            restaurants: restaurantsList,
+            page: page,
+            filters: filters,
+            entries_per_page: restaurantsPerPage,
+            total_results: totalRestaurants
+        };
+        res.json(response);
+    }
+
+    static async apiGetRestaurantById(req, res, next) {
+        try {
+            let id = req.params.id || {};
+            let restaurant = await RestaurantsDAO.getRestaurantById(id);
+            if (!restaurant) {
+                res.status(404).json({ error: 'Not found' });
+                return;
+            }
+            res.json(restaurant);
+        } catch (e) {
+            console.log(`api, ${e}`);
+            res.status(500).json({ error: e });
+        }
     }
 }
